6d6914b9c3b9b0cbf4d97128c3a135e8eec8bcd3
vedina <jeliazkova.nina@gmail.com>
2025-12-13 11:14:37 +0200

    use skip existing parameter

--- tasks/mapiec.py
+++ tasks/mapiec.py
@@ -1,7 +1,7 @@
 import os.path
 import numpy as np
 import pandas as pd
-from tasks.descriptors.ecfp import init_cache, smiles_to_ecfp_cached
+from tasks.descriptors.ecfp import init_cache
 from tasks.mapie_class_ordinal import train_conformal_classifier, predict_conformal_classifier
 from tasks.vega.utils_vega import (
     replace_labels_with_keys, parse_classvalues)
@@ -50,54 +50,56 @@ def clean_classdataset(df, model=None, classvalues_dict=None):
     return cleaned_df, label_pred
 
 
-conn = init_cache(cache_path)
-# --- Load Data ---
-classvalues_dict = get_clas_values(vega_models)
-conn = init_cache(cache_path)
-np.random.seed(42)
-input_file = os.path.join(input_folder, f"{data}.xlsx")
-# Load calibration data
-df_calibration = pd.read_excel(input_file, sheet_name=data)
-df_calibration, label_pred = clean_classdataset(df_calibration, data, classvalues_dict)
-print(label_pred)
-df_calibration.head()
+if skip_existing and os.path.exists(product["ncmodel"]):
+    print(f"CP model exists {product['ncmodel']}")
+    pass
+else:
+    conn = init_cache(cache_path)
+    # --- Load Data ---
+    classvalues_dict = get_clas_values(vega_models)
+    conn = init_cache(cache_path)
+    np.random.seed(42)
+    input_file = os.path.join(input_folder, f"{data}.xlsx")
+    # Load calibration data
+    df_calibration = pd.read_excel(input_file, sheet_name=data)
+    df_calibration, label_pred = clean_classdataset(df_calibration, data, classvalues_dict)
+    print(label_pred)
+    df_calibration.head()
 
+    train_conformal_classifier(
+        df=df_calibration,
+        pred_column=label_pred,
+        cache_path=cache_path,
+        alpha=0.1,
+        output_model_path=product["ncmodel"]
+    )
 
-train_conformal_classifier(
-    df=df_calibration,
-    pred_column=label_pred,
-    cache_path=cache_path,
-    alpha=0.1,
-    output_model_path=product["ncmodel"]
-)
+    test_df = pd.read_excel(input_file, sheet_name=data)
+    test_df, label_pred = clean_classdataset(test_df, data, classvalues_dict)
+    print(label_pred)
+    test_df.head()
 
-test_df = pd.read_excel(input_file, sheet_name=data)
-test_df, label_pred = clean_classdataset(test_df, data, classvalues_dict)
-print(label_pred)
-test_df.head()
+    result_df, metrics_per_model = predict_conformal_classifier(
+        test_df, pred_column=label_pred,
+        true_column="Exp",
+        model_path=product["ncmodel"],
+        tag=data
+    )
 
+    model_metrics = {}
+    model_metrics[data] = metrics_per_model
+    metrics_df = pd.DataFrame.from_dict(model_metrics, orient='index')
+    metrics_df.index.name = 'Method Name'
+    metrics_df["alpha"] = alpha
+    metrics_df
 
-result_df, metrics_per_model = predict_conformal_classifier(
-    test_df, pred_column=label_pred,
-    true_column="Exp",
-    model_path=product["ncmodel"],
-    tag=data
-)
+    output_data_path = product["data"]
+    with pd.ExcelWriter(output_data_path, engine='xlsxwriter') as writer:
+        for sheet in ['Cover sheet', 'Summary sheet']:
+            _df = pd.read_excel(input_file, sheet_name=sheet)
+            _df.to_excel(writer, sheet_name=sheet, index=False)        
+        if result_df is not None:
+            result_df.to_excel(writer, sheet_name='Prediction Intervals', index=False)        
+        metrics_df.to_excel(writer, sheet_name='Metrics') 
 
-model_metrics = {}
-model_metrics[data] = metrics_per_model
-metrics_df = pd.DataFrame.from_dict(model_metrics, orient='index')
-metrics_df.index.name = 'Method Name'
-metrics_df["alpha"] = alpha
-metrics_df
-
-output_data_path = product["data"]
-with pd.ExcelWriter(output_data_path, engine='xlsxwriter') as writer:
-    for sheet in ['Cover sheet', 'Summary sheet']:
-        _df = pd.read_excel(input_file, sheet_name=sheet)
-        _df.to_excel(writer, sheet_name=sheet, index=False)        
-    if result_df is not None:
-        result_df.to_excel(writer, sheet_name='Prediction Intervals', index=False)        
-    metrics_df.to_excel(writer, sheet_name='Metrics') 
-
-print(f"\n✓ Results saved to {product["data"]}")
+    print(f"\n✓ Results saved to {product["data"]}")
